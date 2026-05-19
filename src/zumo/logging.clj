504c3dbe55158f8715f5aa63e394b92cb0b308da
Christian Treppo <christian@treppo.org>
2026-05-19 09:47:13 +0200

    Preserve callsite information in logs
    Signed-off-by: Christian Treppo <christian@treppo.org>

--- src/zumo/logging.clj
+++ src/zumo/logging.clj
@@ -1,6 +1,11 @@
 (ns zumo.logging
-  (:require [taoensso.telemere :as telemere]))
+  (:require [taoensso.telemere :as telemere]
+            [taoensso.truss    :as truss]))
 
-(defn info [message] (telemere/log! :info message))
-(defn error [message] (telemere/log! :error message))
-(defn warn [message] (telemere/log! :warn message))
+;; defined as macros so telemere can print the callsite's line and column
+(defmacro info [message]
+  (truss/keep-callsite `(telemere/log! :info ~message)))
+(defmacro error [message]
+  (truss/keep-callsite `(telemere/log! :error ~message)))
+(defmacro warn [message]
+  (truss/keep-callsite `(telemere/log! :warn ~message)))
