86d63778a7f5f8d598ad265657f182fff3b1f577
Christian Treppo <christian@treppo.org>
2026-05-20 10:28:14 +0200

    Use tools.build to build uberjar
    Signed-off-by: Christian Treppo <christian@treppo.org>

---
+++ build.clj
@@ -0,0 +1,19 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def project-name 'zumo)
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def uber-file (format "target/%s.jar" (name project-name)))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn uberjar [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "resources"], :target-dir class-dir})
+  (b/compile-clj {:basis basis, :src-dirs ["src"], :class-dir class-dir})
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :basis basis
+           :main 'zumo.main}))
--- src/zumo/config.clj
+++ src/zumo/config.clj
@@ -1,7 +1,8 @@
 (ns zumo.config
-  (:require [aero.core :as aero]))
+  (:require [aero.core :as aero]
+            [clojure.java.io :as io]))
 
 (defn load-config []
-  (aero/read-config "config.edn" {:profile (or (keyword (System/getenv "PROFILE")) :production)}))
+  (aero/read-config (io/resource "config.edn") {:profile (or (keyword (System/getenv "PROFILE")) :production)}))
 
 (defn port [config] (:port config))
